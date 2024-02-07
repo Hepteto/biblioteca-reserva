1fec14b90adbde3fef7eb6c1929dd1b89e1e0b3e
Hepteto <136929258+Hepteto@users.noreply.github.com>
2024-02-07 14:12:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
-// config inicial
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const todoRoutes = require('./Routes/todoRoutes');
 
-const dotenv = require('dotenv')
-dotenv.config()
+dotenv.config();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const mongoose = require('mongoose')
-
-const cors = require('cors')
-app.use(cors())
-
-// Forma de ler JSON/ middlewares
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-)
-app.use(express.json())
+// Middleware para lidar com solicitações de /favicon.ico
+app.get('/favicon.ico', (req, res) => res.status(204));
 
 // Rotas da API
-const kindleRoutes = require('./routes/kindleRoutes')
-const salaGrupoRoutes = require('./routes/salaGrupoRoutes')
-
-app.use('/salagrupo', salaGrupoRoutes)
-app.use('/kindle', kindleRoutes)
+app.use('/todo', todoRoutes);
 
 // Rota/ Endpoint inicial
 app.get('/', (req,res) =>{
     res.json({message: 'Testando API'})
-})
+});
 
 // entregar uma porta
-// mongoose.connect( process.env.MONGO ) // conectar ao banco de dados
-mongoose.connect( process.env.MONGO ) // conectar ao banco de dados
-.then(() => {
-    console.log("Conectamos ao mongo DB")
-    app.listen(3000) // Vai ler a porta
-})
-.catch((err) => console.log(err))
+mongoose.connect(process.env.MONGO) // conectar ao banco de dados
+    .then(() => {
+        console.log("Conectamos ao mongo DB");
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+    })
+    .catch((err) => console.log(err));
